e115ba1a7772b8ec22279817abd6f53c5cb82137
Lauren Barker <lauren@cos.io>
2017-01-25 17:04:04 -0500

    Read in data

--- sharepush/data.py
+++ sharepush/data.py
@@ -1,12 +1,20 @@
 import os
+import csv
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 def get_data():
-    # read in contributors
-    # read in awards
-    # read in funders
-    # read in works
-    pass
+    """ Return dictionaries for each file in the data dir
+    """
+    data = {}
+    for filename in os.listdir('data'):
+        with open(os.path.join(os.path.dirname((os.path.dirname(__file__))), 'data/') + filename, 'r') as f:
+            reader = csv.reader(f)
+            headers = next(reader)
+            rows = list(reader)
+            file_data = {row.pop(0): row for row in rows}
+            file_data['headers'] = headers
+            data[str(os.path.splitext(filename)[0])] = file_data
+    return data
